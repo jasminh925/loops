fa26d756d5fba5625ebd1bb9856287318965b100
Your Name <you@example.com>
2016-08-04 21:37:24 -0700

    loops activity

---
+++ loopy.py
@@ -0,0 +1,13 @@
+# for i in range(1,21):
+# 	if i == 13:
+# 		print "hello",
+# 	else:
+# 		print i,
+
+basket = ['apples', 'oranges', 'bananas']
+
+# for fruit in basket:
+# 	print fruit
+
+for index in range(len(basket)):
+	print basket[index]
---
+++ while_fun_loops.py
@@ -0,0 +1,28 @@
+# count = 1
+# while(count<21):
+# 	if count == 13:
+# 		print "hello"
+# 	else:
+# 		print count
+# 	count = count + 1
+
+# count = 0
+# while count<101:
+#  	if count == 0 or (count % 2 == 0):
+#  		print count
+#  		count = count + 1
+#  	else:
+#  		print count	
+#  	count = count + 1
+
+# count = 0
+# while count < 101:
+# 	print count
+# 	count = count + 10
+
+
+basket = ['apples', 'oranges', 'bananas']
+index = 0
+while index < len(basket):
+	print basket[index]
+	index = index + 1
